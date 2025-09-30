33072004a94c621569acab7cc3d4e3c0ddab3798
RidgeG <ridgegeervliet@gmail.com>
2025-09-30 12:38:29 +0200

    first commit

---
+++ src/main/java/com/ridge/geervliet/novi/dekookassistent/DeKookAssistentApplication.java
@@ -0,0 +1,13 @@
+package com.ridge.geervliet.novi.dekookassistent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeKookAssistentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeKookAssistentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ridge/geervliet/novi/dekookassistent/DeKookAssistentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridge.geervliet.novi.dekookassistent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeKookAssistentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
